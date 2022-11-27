f8a1088a1375745c7b02b21bbe2504cce358a365
motoki <kounotori100588@i.softbank.jp>
2022-11-27 23:47:11 +0900

    backip

--- src/main/java/com/example/TaskApplication.java
+++ src/main/java/com/example/TaskApplication.java
@@ -3,17 +3,19 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TaskApplication {
+public class TaskApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskApplication.class, args);
 	}
-
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(TaskApplication.class);
-	}
+	
+	@Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		    return application.sources(TaskApplication.class);
+	  }
 	
 }
 
